f5fe1ff82d2ca56f58c08807be09d3cc599ba454
pranaliprixim <pranalip.prixim@gmail.com>
2024-03-26 12:21:55 +0530

    any

--- stocks.php
+++ stocks.php
@@ -1,2 +1,50 @@
 <?php include("includes/header.php"); ?>
+
+<section class="banner-section  pt-120 pb-120">
+        <div class="container mt-lg-0 pt-18 pt-xl-20">
+            <div class="row">
+                <div class="col-12 breadcrumb-area ">
+                    <h2 class="mb-4">About Us</h2>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb mb-0">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item ms-2 ps-7 active" aria-current="page"><span>About Us</span></li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="trade_on a2-bg pt-120 pb-120 position-relative z-0">
+        <div class="animation vector position-absolute top-0 left-0 w-100 h-100 z-n1">
+            <img src="assets/images/star3.png" alt="vector" class="position-absolute top-0 end-0 pe-15 pt-20 push_animat d-none d-xxxl-flex">
+            <img src="assets/images/vector.png" alt="vector" class="position-absolute top-0 start-0 pt-20 ms-20 jello d-none d-xxxl-flex">
+            <img src="assets/images/vector5.png" alt="vector" class="position-absolute bottom-0 end-0 d-none d-lg-flex previewShapeRevX ">
+        </div>
+        <div class="container">
+            <div class="row gy-10 gy-xxl-0 justify-content-center justify-content-xxl-between align-items-center">
+                <div class="col-10 col-sm-8 col-md-7 col-lg-6 order-2 order-lg-0">
+                    <div class="trade_on__thumbs d-flex justify-content-end">
+                        <img src="assets/images/trade_on2.png" alt="Imgae">
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xxl-5">
+                    <div class="trade_on__content">
+                        <span class="heading p1-color fs-five mb-5">Trade On Our</span>
+                        <h3 class="mb-4 mb-lg-5">World Class Platform</h3>
+                        <p class="fs-six mx-ch">Trading in financial markets involves a wide range of strategies that traders employ to make informed decisions. From trading to swing trading and long-term investing, each strategy has its own set of principles and risk factors.</p>   
+                        <ul class="d-flex gap-4 flex-column mt-6">
+                            <li class="d-flex align-items-center gap-3 fs-six-up"><i class="ti ti-circle-check p1-color fs-four"></i>Charts trading</li>
+                            <li class="d-flex align-items-center gap-3 fs-six-up"><i class="ti ti-circle-check p1-color fs-four"></i>Understanding Trading Strategies </li>
+                            <li class="d-flex align-items-center gap-3 fs-six-up"><i class="ti ti-circle-check p1-color fs-four"></i>Risk Management in Trading </li>
+                            <li class="d-flex align-items-center gap-3 fs-six-up"><i class="ti ti-circle-check p1-color fs-four"></i>Technical vs. Fundamental Analysis </li>
+                        </ul>
+                        <a href="signup.html" class="cmn-btn fs-six-up nb4-xxl-bg gap-2 gap-lg-3 align-items-center py-2 px-3 py-lg-3 px-lg-4 mt-7 mt-xxl-8">Sign up Now <i class="ti ti-arrow-right fs-four"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 <?php include("includes/footer.php"); ?>
