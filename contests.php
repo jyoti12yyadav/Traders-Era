efd973dacb2ff0882244cc280a16c1724cc19aed
Ziasolkar <ziaprixim@gmail.com>
2024-03-27 15:56:36 +0530

    replace

--- contests.php
+++ contests.php
@@ -1,2 +1,21 @@
 <?php include("includes/header.php"); ?>
+
+<section class="banner-section  pt-120 pb-120">
+    <div class="container mt-lg-0 pt-18 pt-xl-20">
+        <div class="row">
+            <div class="col-12 breadcrumb-area ">
+                <h2 class="mb-4">Contests</h2>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-0">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item ms-2 ps-7 active" aria-current="page"><span>Contests</span></li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
 <?php include("includes/footer.php"); ?>
