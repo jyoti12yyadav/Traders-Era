b51e8259543e98efaf1044d200dee5cb5951b072
pranaliprixim <pranalip.prixim@gmail.com>
2024-03-27 11:09:15 +0530

    any

--- forex.php
+++ forex.php
@@ -4,11 +4,11 @@
         <div class="container mt-lg-0 pt-18 pt-xl-20">
             <div class="row">
                 <div class="col-12 breadcrumb-area ">
-                    <h2 class="mb-4">About Us</h2>
+                    <h2 class="mb-4">Forex</h2>
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb mb-0">
                             <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                            <li class="breadcrumb-item ms-2 ps-7 active" aria-current="page"><span>About Us</span></li>
+                            <li class="breadcrumb-item ms-2 ps-7 active" aria-current="page"><span>Forex</span></li>
                         </ol>
                     </nav>
                 </div>
