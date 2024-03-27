b3fb637a0706f6299bf445c5db2414d45b71c111
Ziasolkar <ziaprixim@gmail.com>
2024-03-27 15:33:59 +0530

    replace

--- offers.php
+++ offers.php
@@ -1,2 +1,102 @@
 <?php include("includes/header.php"); ?>
+<section class="banner-section pt-120 pb-120">
+    <div class="container mt-10 mt-lg-0 pt-15 pt-lg-20 pb-5 pb-lg-0">
+        <div class="row">
+            <div class="col-12 breadcrumb-area ">
+                <h2 class="mb-4">Withdrawal</h2>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-0">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item ms-2 ps-7 active" aria-current="page"><span>Withdrawal</span></li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="trade_on a2-bg pt-120 pb-120 position-relative z-0">
+    <div class="animation position-absolute top-0 left-0 w-100 h-100 z-n1">
+        <img src="assets/images/coin.png" alt="vector" class="position-absolute d-none d-md-flex previewShapeRevX">
+        <img src="assets/images/star2.png" alt="vector" class="position-absolute d-none d-xl-flex push_animat">
+        <img src="assets/images/coin_vector.png" alt="vector"
+            class="position-absolute d-none d-xxxl-flex bottom-0 end-0 previewShapeRevX opacity-50">
+    </div>
+    <div class="container">
+        <div class="row gy-10 gy-xxl-0 justify-content-center justify-content-xxl-between align-items-center">
+            <div class="col-lg-6 col-xxl-5">
+                <div class="trade_on__content">
+                    <span class="heading p1-color fs-six mb-5" style="margin-top:120px;">Get a 40% Bonus and trade
+                        wisely </span>
+                    <!-- <span class="heading s1-color fs-five mb-5">Trade On Our</span> -->
+                    <h3 class="mb-4 mb-lg-5"> Unlock Confidence and Success in Financial Markets with Us</h3>
+                    <p class="fs-six mx-ch">Boost your trading margin with our incredible 40% deposit bonus, allowing
+                        you to expand your positions and enhance your trading strategy.</p>
+                    <br>
+                    <p class="fs-six mx-ch">Think of this bonus as your shield during tough times! Even if you
+                        experience losses and use up your deposited funds, this bonus remains untouched, ensuring you
+                        can keep on trading without any worries or interruptions </p>
+                    <br>
+                    <p class="fs-six mx-ch">Get a Bonus to Supercharge Your Trading Potential! It increases your initial
+                        investment, opening doors to bigger financial gains on your successful trades.</p>
+                    <br>
+                    <p class="fs-six mx-ch">Factors that can affect the price of cryptocurrencies are their supply and
+                        market demands, regulation, news and media, and price speculation.</p>
+                    <br>
+                    <p class="fs-six mx-ch">Get a Boost with Our 40% Bonus – Trade with Confidence Even During Market
+                        Ups and Downs!</p>
+                </div>
+            </div>
+            <div class="col-md-8 col-lg-6">
+                <div class="trade_on__thumbs d-flex justify-content-end">
+                    <img src="assets/images/trade_on.png" alt="Imgae">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="market-details pt-120  position-relative z-0">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-10">
+                <h3 class=" mb-lg-8">Vital details:</h3>
+                <div class="market-details__card">
+                    <div class="market-details__content d-flex flex-column gap-5 gap-lg-6 mt-5 mt-lg-6">
+                        <p>Trading Bonus Exclusive: Trade freely and Withdraw Profits Hassle-free.
+                            Enjoy a 40% Bonus on Your First Deposit, and an Additional 25% Bonus on Subsequent Deposits.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="market-details pt-120  position-relative z-0">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-10">
+                <h3 class="mb-7 mb-lg-8"> Seize this opportunity before it's gone!</h3>
+                <div class="market-details__card">
+                    <div class="market-details__thumbs position-relative">
+                        <img src="assets/images/market_details.png" alt="Image" class="cus-rounded-2 w-100">
+                    </div>
+                    <div class="market-details__content d-flex flex-column gap-5 gap-lg-6 mt-5 mt-lg-6">
+                        <p>Looking to stay updated on all the fantastic bonuses coming your way? Be sure to subscribe to
+                            our newsletter and follow us on social media. By becoming a part of our community, you'll
+                            receive exclusive early access to bonus announcements, ensuring you're always in the loop
+                            and never miss out on exciting opportunities to enhance your trading experience.</p>
+                        <p>We understand trading is more than just making money. It's a passion, a journey full of highs
+                            and lows that demands commitment and assistance. That's why we're excited to introduce
+                            upcoming bonuses that will not only enhance your potential but also bring excitement to
+                            every trade. Prepare yourself for a trading atmosphere brimming with enthusiasm and
+                            anticipation.</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<br>
 <?php include("includes/footer.php"); ?>
