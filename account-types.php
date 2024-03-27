97032d933eff4808c23d157aa8a6b3d1cdfbe52b
Ziasolkar <ziaprixim@gmail.com>
2024-03-27 12:57:37 +0530

    replace

--- account-types.php
+++ account-types.php
@@ -1,2 +1,43 @@
 <?php include("includes/header.php"); ?>
+<section class="banner-section pt-120 pb-120">
+    <div class="container mt-10 mt-lg-0 pt-15 pt-lg-20 pb-5 pb-lg-0">
+        <div class="row">
+            <div class="col-12 breadcrumb-area ">
+                <h2 class="mb-4">Market</h2>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-0">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item ms-2 ps-7 active" aria-current="page"><span>Market</span></li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="container">
+    <div class="row gy-10 gy-xxl-0 justify-content-center justify-content-xxl-between align-items-center">
+        <div class="col-10 col-sm-8 col-md-7 col-lg-6 order-2 order-lg-0">
+            <div class="trade_on__thumbs d-flex justify-content-end">
+                <img src="assets/images/education.png" alt="Imgae">
+            </div>
+        </div>
+        <div class="col-md-10 col-lg-6 col-xxl-5">
+            <div class="trade_on__content">
+                <span class="heading p1-color fs-six mb-5 mt-6">TradersEra ECN</span>
+                <h4 class="mb-4 mb-lg-5">TradersEra provides two different account options, each designed to meet
+                    specific trading needs.</h4>
+                <p class="mx-ch">We offer competitive spreads starting from 0.0 pips on forex and other popular
+                    instruments. You'll be pleased to know that our commission starts from just $35 per $1M traded. For
+                    more information on commission details, please refer to the account types table below.</p>
+                <div class="d-flex gap-3 align-items-center"> 
+                    <ul class=" d-flex flex-wrap gap-5  mt-5 mb-5">
+                        <li class="d-flex align-items-center gap-3 fs-six-up"><i class="ti ti-circle-check s1-color fs-four"></i>Ideal for scalpers and day traders, tighter spreads are available.</li>
+                        <li class="d-flex align-items-center gap-3 fs-six-up"><i class="ti ti-circle-check s1-color fs-four"></i> Gain access to direct market with transparent pricing and quicker execution.</li>
+                        <li class="d-flex align-items-center gap-3 fs-six-up"><i class="ti ti-circle-check s1-color fs-four"></i>Enjoy easy entry and exit in volatile markets with high liquidity.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php include("includes/footer.php"); ?>
