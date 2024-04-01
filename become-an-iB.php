09207985d4ca087ab866ff387da335bc99f7369e
Ziasolkar <ziaprixim@gmail.com>
2024-04-01 15:04:02 +0530

    replace

--- become-an-iB.php
+++ become-an-iB.php
@@ -4,11 +4,12 @@
     <div class="container mt-10 mt-lg-0 pt-15 pt-lg-20 pb-5 pb-lg-0">
         <div class="row">
             <div class="col-12 breadcrumb-area ">
-                <h2 class="mb-4">Become an IB</h2>
+                <h2 class="mb-4">Become an Partner</h2>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0">
                         <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                        <li class="breadcrumb-item ms-2 ps-7 active" aria-current="page"><span>Become an IB</span></li>
+                        <li class="breadcrumb-item ms-2 ps-7 active" aria-current="page"><span>Become an Partner</span>
+                        </li>
                     </ol>
                 </nav>
             </div>
@@ -17,55 +18,80 @@
 </section>
 
 
-<section class="market-details pt-120 pb-120 position-relative z-0">
+<section class="trade_on trade_on--secondary pt-120 pb-120 position-relative z-0">
+    <div class="animation position-absolute top-0 left-0 w-100 h-100 z-n1">
+        <img src="assets/images/star3.png" alt="vector"
+            class="position-absolute push_animat d-none d-xxxl-flex top-0 start-0 pt-lg-16 ps-20 ms-lg-8">
+        <img src="assets/images/sun2.png" alt="vector"
+            class="sun2 position-absolute push_animat d-none d-xxl-flex bottom-0 pb-120">
+    </div>
     <div class="container">
-        <div class="row justify-content-center">
-        <h2 class="d-flex justify-content-center mb-10">Become An IB</h2>
-            <div class="col-lg-10">
-                <div class="market-details__card">
-                    <div class="market-details__thumbs position-relative">
-                        <img src="assets/images/become an ib section 1.png" alt="Image" class="cus-rounded-2 w-100">
-                    </div>
-                    <div class="market-details__content d-flex flex-column gap-5 gap-lg-6 mt-5 mt-lg-6">
-                        <p>The TradersEra IB program is designed to give both corporate and individual clients the chance
-                            to profit continuously from their clients' trading activity. We provide IBs with some of the
-                            greatest rates in the business because we appreciate the business you bring. </p>
-                        <div class="privacy-policy__part">
-                            <h5 class="mb-4">1. Magnificent Commission Framework:</h5>
-                            <p class="mt-4 mb-5">You can profit from a very competitive commission structure as an
-                                Introducing Broker (IB) with TradersEra, which enables you to make large commissions on
-                                the trading activity of clients you suggest. Our clear-cut and generous commission
-                                structure guarantees that you will receive just compensation for your work, giving you a
-                                sizable source of income.
-                            </p>
+        <div class="row gy-10 gy-xxl-0 justify-content-center justify-content-xxl-between align-items-center">
 
-                            <h5 class="mb-4">2. All-Inclusive Assistance and Materials:</h5>
-                            <p class="mt-4 mb-5">To assist you in becoming a successful IB, TradersEra provides extensive
-                                support and tools. We provide you with the tools and support you need to successfully
-                                sell our services and draw in customers, from marketing materials and instructional
-                                materials to specific assistance from our committed support team. We're not just here to
-                                provide a platform for your achievement; we're here to empower you at every turn.
-                            </p>
+            <div class="col-md-10 col-lg-6 col-xxl-5">
+                <div class="trade_on__content">
+                    <h4 class="mb-4 mb-lg-5">Become a Partner Today !</h4>
+                    <p class="mx-ch">Are your friends always seeking your trading advice? Do you have an online
+                        community? It's time to turn your knowledge into cash! Let's make your insights work for you</p>
+                    <br>
+                    <p>
+                        Withdraw anytime with minimum processing fee.
+                    </p>
+                    <a href="signup.html"
+                        class="cmn-btn fs-six-up nb4-xxl-bg gap-2 gap-lg-3 align-items-center py-2 px-4 py-lg-3 px-lg-5 mt-7 mt-xxl-8 "
+                        style="font-weight: bold; --top: 27.09375px; --left: 52.5625px;">Become a Partner</a>
+                </div>
+            </div>
 
-                            <h5 class="mb-4">3.Strong Technology and Trading Environment:</h5>
-                            <p class="mt-4 mb-5">Working with TradersEra gives you access to a trading environment that
-                                is both strong and modern. Our platform's innovative features, such as real-time data,
-                                adaptable trading tools, and secure infrastructure, guarantee your recommended clients a
-                                flawless trading experience. You can improve customer retention and happiness by
-                                providing an excellent trading environment, which can ultimately propel your IB firm to
-                                new heights.
-                            </p>
-                            <p class="mt-4 mb-5">Join TradersEra as an IB to take advantage of the possibility of large
-                                profits, all-encompassing support, and entry into a top-notch trading environment. Join
-                                us in influencing the direction of forex trading as we begin a successful partnership
-                                with TradersEra.
-                            </p>
-                        </div>
-                    </div>
+            <div class="col-10 col-sm-8 col-md-7 col-lg-6 order-2 order-lg-0">
+                <div class="trade_on__thumbs d-flex justify-content-end">
+                    <img src="assets/images/withdraw patners.png" alt="Imgae">
                 </div>
             </div>
         </div>
     </div>
 </section>
 
+<section class="provide-world pt-120 pb-120  position-relative z-0">
+    <div class="container">
+        <div class="row gy-6 gy-xxl-0">
+            <h3 class=" mb-lg-5 d-flex justify-content-center" style="important;">WHAT WE OFFER</h3>
+           <center><h5 class="mb-7">Join our Partner Program and Enjoy Exclusive Perks</h5></center>
+            <div class="col-md-6 col-xxl-4">
+                <div class="provide-world__card secondary nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                    <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
+                    <img src="assets/images/icon/distribution.png" alt="stock-market">
+                    </span>
+                    <h4 class="mt-5 mb-5">Customized Commission Plans</h4>
+                    <p> We offer commission structures at TradersEra that are tailored to meet your specific requirements, ensuring a mutually beneficial partnership..</p>
+                    <br>
+                </div>
+            </div>
+            <div class="col-md-6 col-xxl-4">
+                <div class="provide-world__card secondary nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                    <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
+                        <img src="assets/images/icon/check.png" alt="stock-market">
+                    </span>
+                    <h4 class="mt-5 mb-5">Timely Payouts</h4>
+                    <p>You can rely on our timely payments and commission rates to provide you with a steady income stream that you can count on.</p>
+                    <br>
+                    <br>
+                    <br>
+                </div>
+            </div>
+            <div class="col-md-6 col-xxl-4">
+                <div class="provide-world__card secondary nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                    <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
+                        <img src="assets/images/icon/withdrawal.png" alt="stock-market">
+                    </span>
+                    <h4 class="mt-5 mb-5">Cutting Edge Technological Solutions</h4>
+                    <p>Get access to cutting-edge tools and support to enhance your trading journey. Our friendly team is ready to assist you at every step.</p>
+                    <br>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</section>
 <?php include("includes/footer.php"); ?>
