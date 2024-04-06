2174cab901b5fce15ffea0886b26d3b18a626cd5
pranaliprixim <pranalip.prixim@gmail.com>
2024-04-06 14:26:54 +0530

    any

--- offers.php
+++ offers.php
@@ -14,86 +14,72 @@
         </div>
     </div>
 </section>
-<!-- <section class="trade_on a2-bg pt-120 pb-120 position-relative z-0">
-    <div class="animation position-absolute top-0 left-0 w-100 h-100 z-n1">
-        <img src="assets/images/coin.png" alt="vector" class="position-absolute d-none d-md-flex previewShapeRevX">
-        <img src="assets/images/star2.png" alt="vector" class="position-absolute d-none d-xl-flex push_animat">
-        <img src="assets/images/coin_vector.png" alt="vector"
-            class="position-absolute d-none d-xxxl-flex bottom-0 end-0 previewShapeRevX opacity-50">
-    </div>
-    <div class="container">
-        <div class="row gy-10 gy-xxl-0 justify-content-center justify-content-xxl-between align-items-center">
-            <div class="col-lg-6 col-xxl-5">
-                <div class="trade_on__content">
-                    <span class="heading p1-color fs-six mb-5" style="margin-top:120px;"> </span>
-                   
-                    <h3 class="mb-4 mb-lg-5"> Unlock Confidence and Success in Financial Markets with Us</h3>
-                    <p class="fs-six mx-ch">Boost your trading margin with our incredible deposit bonus, allowing
-                        you to expand your positions and enhance your trading strategy.</p>
-                    <br>
-                    <p class="fs-six mx-ch">Think of this bonus as your shield during tough times! Even if you
-                        experience losses and use up your deposited funds, this bonus remains untouched, ensuring you
-                        can keep on trading without any worries or interruptions </p>
-                    <br>
-                    <p class="fs-six mx-ch">Get a Bonus to Supercharge Your Trading Potential! It increases your initial
-                        investment, opening doors to bigger financial gains on your successful trades.</p>
-                    <br>
-                    <p class="fs-six mx-ch">Factors that can affect the price of cryptocurrencies are their supply and
-                        market demands, regulation, news and media, and price speculation.</p>
-                    <br>
-                    <p class="fs-six mx-ch"></p>
+
+    <section class="company-story position-relative z-0  pt-120 pb-120 ">
+        <div class="animation position-absolute w-100 h-100 z-n1">
+            <img src="assets/images/star3.png" alt="vector" class="position-absolute top-0 end-0 pt-10 pe-20 me-20 d-none d-xxl-flex previewSkew">
+        </div>
+        <div class="container">
+            <div class="row gy-15 gy-lg-0 justify-content-center align-items-center">
+                <div class="col-sm-10 col-lg-6 col-xxl-3 order-2 order-lg-0">
+                    <div class="company-story__thumbs d-center">
+                        <img src="assets/images/company_story.png" class="cus-rounded-1 w-100" alt="Imgae">
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-8 col-lg-6">
-                <div class="trade_on__thumbs d-flex justify-content-end">
-                    <img src="assets/images/unlock_offers.png" alt="Imgae">
+                <div class="col-lg-6 col-xxl-7">
+                    <div class="row ms-xl-3 ms-xxl-10">
+                        <div class="col-md-6 col-xxl-12">
+                            <div class="provide-world__card nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                               <h4 class="mt-5 mb-5">MetaTrader 5</h4>
+                               <p>For smart tech trading</p>
+                            </div>
+                        </div> 
+                        <div class="col-xxl-12 mt-8 mt-md-10 mt-xxl-13">
+                            <div class="col-md-6 col-xxl-12">
+                                <div class="provide-world__card nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                                   <h4 class="mt-5 mb-5">MetaTrader 4</h4>
+                                   <p>For an easy start</p>
+                                </div>
+                            </div>  
+                        </div>
+                    </div>  
                 </div>
             </div>
         </div>
-    </div>
-</section> -->
+    </section>
 
-<!-- <section class="market-details pt-120  position-relative z-0">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-10">
-                <h3 class=" mb-lg-8">Vital details:</h3>
-                <div class="market-details__card">
-                    <div class="market-details__content d-flex flex-column gap-5 gap-lg-6 mt-5 mt-lg-6">
-                        <p>Trading Bonus Exclusive: Trade freely and Withdraw Profits Hassle-free.
-                            Enjoy as Bonus on Your First Deposit, and an Additional Bonus on Subsequent Deposits.
-                        </p>
+    <section class="company-story position-relative z-0  pt-120 pb-120 ">
+        <div class="animation position-absolute w-100 h-100 z-n1">
+            <img src="assets/images/star3.png" alt="vector" class="position-absolute top-0 end-0 pt-10 pe-20 me-20 d-none d-xxl-flex previewSkew">
+        </div>
+        <div class="container">
+            <div class="row gy-15 gy-lg-0 justify-content-center align-items-center">
+                <div class="col-sm-10 col-lg-6 col-xxl-3 order-2 order-lg-0">
+                    <div class="company-story__thumbs d-center">
+                        <img src="assets/images/company_story.png" class="cus-rounded-1 w-100" alt="Imgae">
                     </div>
+                </div>
+                <div class="col-lg-6 col-xxl-7">
+                    <div class="row ms-xl-3 ms-xxl-10">
+                        <div class="col-md-6 col-xxl-12">
+                            <div class="provide-world__card nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                               <h4 class="mt-5 mb-5">MetaTrader 5</h4>
+                               <p>For smart tech trading</p>
+                            </div>
+                        </div> 
+                        <div class="col-xxl-12 mt-8 mt-md-10 mt-xxl-13">
+                            <div class="col-md-6 col-xxl-12">
+                                <div class="provide-world__card nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                                   <h4 class="mt-5 mb-5">MetaTrader 4</h4>
+                                   <p>For an easy start</p>
+                                </div>
+                            </div>  
+                        </div>
+                    </div>  
                 </div>
             </div>
         </div>
-    </div>
-</section> -->
+    </section>
 
-<!-- <section class="market-details pt-120  position-relative z-0">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-10">
-                <h3 class="mb-7 mb-lg-8 text-center"> Seize this opportunity before it's gone!</h3>
-                <div class="market-details__card">
-                    <div class="market-details__thumbs position-relative">
-                        <img src="assets/images/offers.png" alt="Image" class="cus-rounded-2 w-100">
-                    </div>
-                    <div class="market-details__content d-flex flex-column gap-5 gap-lg-6 mt-5 mt-lg-6">
-                        <p>Join our newsletter and follow us on social media By joining our network, you'll get unique early access to bonus bulletins, so you're  continuously being aware and by no means leave out opportunities to make your buying and selling experience even better
-.</p>
-                        <p>Trading goes beyond just making money. It's a thrilling journey of highs and lows that demands commitment and encouragement. We are excited to share news about upcoming bonuses that will not only enhance your trading capabilities but also inject vitality into each trade. Brace yourself for a trading atmosphere brimming with enthusiasm and thrill.</p>
 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section> -->
-<div class="row">
-    <div class="col-lm-12" style="padding-top:100px; padding-bottom: 100px;">
-        <center><h1 style="font-size: 100px;">Coming Soon</h1></center>
-    </div>
-</div>
-<br>
 <?php include("includes/footer.php"); ?>
