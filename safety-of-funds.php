1c72eaa669a3e2a6addf9f56a8392820955ea8e9
pranaliprixim <pranalip.prixim@gmail.com>
2024-04-08 13:04:43 +0530

    content

--- safety-of-funds.php
+++ safety-of-funds.php
@@ -38,7 +38,7 @@
                         at all times.
                     </p>
                     <br>
-                   
+
                 </div>
             </div>
             <div class="col-md-6 col-xxl-4">
@@ -53,7 +53,7 @@
                         additional layer of protection for your funds and reinforces our commitment to transparency and
                         integrity.
                     </p>
-                
+
 
                 </div>
             </div>
@@ -79,52 +79,57 @@
 
 </section>
 
-<section class="provide-world pt-90 mb-90 position-relative z-0" style="padding-bottom: 25px; margin-bottom: 90px;">
+
+<section class="provide-world pt-90 pb-120  position-relative z-0" style="padding-bottom: 25px; margin-bottom: 90px;">
     <div class="container">
         <div class="row gy-6 gy-xxl-0">
 
-            <div class="col-md-6 col-xxl-6">
-                <div class="provide-world__card secondary nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
-                    <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
-                        <img src="assets/images/icon/Spreads.png" alt="stock-market">
-                    </span>
-                    <h4 class="mt-5 mb-5">Risk Management Protocols
-                    </h4>
-                    <p>TradersEra employs rigorous risk management protocols to mitigate potential risks and protect
-                        against market volatility. Our experienced risk management team continuously monitors market
-                        conditions and implements proactive measures to safeguard your funds against adverse market
-                        movements.
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                </div>
+            <div class="col-md-2 col-xxl-2">
+
+
+
+
             </div>
-            <div class="col-md-6 col-xxl-6">
+
+            <div class="col-md-5 col-xxl-4">
                 <div class="provide-world__card secondary nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
                     <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
                         <img src="assets/images/icon/commission.png" alt="stock-market">
                     </span>
-                    <h4 class="mt-5 mb-5">Client Fund Insurance
+                    <h4 class="mt-5 mb-5">Regulatory Compliance
                     </h4>
-                    <p>Addition to our own security measures, TradersEra may also offer client fund insurance through
-                        reputable third-party providers. This insurance coverage provides an extra layer of protection
-                        for your funds in the unlikely event of insolvency or financial distress.
-
-                        With TradersEra , you can trade with confidence, knowing that your funds are secure and
-                        protected. Our unwavering commitment to the safety and security of your investments underscores
-                        our dedication to providing you with a trustworthy and reliable trading environment. Trade with
-                        peace of mind at TradersEra , where your financial security is our top priority.
+                    <p>TradersEra operates under the oversight of reputable regulatory authorities, ensuring compliance
+                        with strict regulatory standards. Our adherence to regulatory requirements provides an
+                        additional layer of protection for your funds and reinforces our commitment to transparency and
+                        integrity.
                     </p>
 
+
                 </div>
+            </div>
+            <div class="col-md-5 col-xxl-4">
+                <div class="provide-world__card secondary nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                    <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
+                        <img src="assets/images/icon/Low-starting-amount.png" alt="stock-market">
+                    </span>
+                    <h4 class="mt-5 mb-5">Secure Payment Gateways
+                    </h4>
+                    <p>We utilize cutting-edge encryption technology and secure payment gateways to protect your
+                        financial transactions and personal information. Our robust security measures safeguard against
+                        unauthorized access and ensure the confidentiality and integrity of your data.
+                    </p>
+
+                    <br>
+                </div>
+            </div>
+            <div class="col-md-2 col-xxl-1">
+              
             </div>
 
         </div>
     </div>
 
 </section>
+
 
 <?php include("includes/footer.php"); ?>
