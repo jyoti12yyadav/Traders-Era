59a5d5f3d4da3178e627edd9f233bb23c275e0b3
pranaliprixim <pranalip.prixim@gmail.com>
2024-04-09 16:05:55 +0530

    any

--- kyc-checks.php
+++ kyc-checks.php
@@ -1,18 +1,212 @@
 <?php include("includes/header.php"); ?>
+
+<style>
+    .table-para {
+        background-color: #292929;
+        padding: 35px;
+        border: 1px solid #838383;
+        border-radius: 14px;
+        margin: 10px 0px;
+    }
+
+    tr {
+        text-align: center;
+    }
+</style>
+
 <section class="banner-section  pt-120 pb-120">
     <div class="container mt-10 mt-lg-0 pt-15 pt-lg-20 pb-5 pb-lg-0">
         <div class="row">
             <div class="col-12 breadcrumb-area ">
-                <h2 class="mb-4">Fx CFD</h2>
+                <h2 class="mb-4">KYC Checks</h2>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0">
                         <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                        <li class="breadcrumb-item ms-2 ps-7 active" aria-current="page"><span>Fx CFD</span></li>
+                        <li class="breadcrumb-item ms-2 ps-7 active" aria-current="page"><span>KYC Checks</span></li>
                     </ol>
                 </nav>
             </div>
         </div>
     </div>
 </section>
+
+
+<section class="trade_on trade_on--secondary pt-120 pb-120 position-relative z-0">
+    <div class="animation position-absolute top-0 left-0 w-100 h-100 z-n1">
+        <img src="assets/images/star3.png" alt="vector"
+            class="position-absolute push_animat d-none d-xxxl-flex top-0 start-0 pt-lg-16 ps-20 ms-lg-8">
+    </div>
+    <div class="container">
+        <div class="row gy-10 gy-xxl-0 justify-content-center justify-content-xxl-between align-items-center">
+
+
+            <div class="col-md-10 col-lg-6 col-xxl-5">
+                <div class="trade_on__content">
+                    <h2 class="mb-4 mb-lg-5" style="font-size:35px;">KYC Checks</h2>
+                </div>
+
+
+
+                <ul class="d-flex gap-4 flex-column mt-6">
+                    <li class="d-flex align-items-center gap-3 fs-six-up"><i
+                            class="ti ti-circle-check s1-color fs-four"></i>Verify the identity of customers through
+                        government-issued identification documents.
+                    </li>
+                    <li class="d-flex align-items-center gap-3 fs-six-up"><i
+                            class="ti ti-circle-check s1-color fs-four"></i>Confirm the authenticity of provided
+                        documents through manual review or automated verification systems.
+                    </li>
+                    <li class="d-flex align-items-center gap-3 fs-six-up"><i
+                            class="ti ti-circle-check s1-color fs-four"></i>Conduct due diligence checks to ensure
+                        compliance with regulatory requirements and prevent fraud.
+                    </li>
+                    <li class="d-flex align-items-center gap-3 fs-six-up"><i
+                            class="ti ti-circle-check s1-color fs-four"></i>Validate the residential address of customers by cross-referencing their identification documents with bank statements, or other official documents.
+                    </li>
+                    <li class="d-flex align-items-center gap-3 fs-six-up"><i
+                            class="ti ti-circle-check s1-color fs-four"></i>Screen customers against global watchlists and sanctions databases to ensure they are not involved in any illegal activities.
+                    </li>
+                    <li class="d-flex align-items-center gap-3 fs-six-up"><i
+                        class="ti ti-circle-check s1-color fs-four"></i>Conduct enhanced due diligence for high-risk customers or transactions, which may involve gathering additional information, performing background checks, or seeking approval from senior management.
+                </li>
+                </ul>
+
+            </div>
+            <div class="col-10 col-sm-8 col-md-7 col-lg-6 order-2 order-lg-0">
+                <div class="trade_on__thumbs d-flex justify-content-end">
+                    <img src="assets/images/KYC checks.png" alt="Imgae">
+                </div>
+            </div>
+
+        </div>
+</section>
+
+<section class="company-story position-relative z-0  pt-120 pb-120 ">
+    <div class="animation position-absolute w-100 h-100 z-n1">
+        <img src="assets/images/star3.png" alt="vector"
+            class="position-absolute top-0 end-0 pt-10 pe-20 me-20 d-none d-xxl-flex previewSkew">
+    </div>
+    <div class="container">
+        <div class="row gy-15 gy-lg-0 justify-content-center align-items-center">
+            <div class="container my-5">
+                <table id="Record" class="table table-striped table-dark my-3">
+                    <thead>
+                        <tr>
+                            <th scope="col"
+                                style="background-color: #9ad953;color: black;text-align: center;font-size: 16px;padding:20px;display: table-cell;">
+                                KYC Checks</th>
+                            <th scope="col"
+                                style="background-color: #F5C06D;color: black;text-align: center;font-size: 16px;padding:20px;display: table-cell;">
+                                Description</th>
+                        </tr>
+                    </thead>
+                    <tbody class="justify-content-center text-start">
+                        <tr>
+                            <td>Identity Verification</td>
+                            <td>Verify customers' identities using government-issued ID documents such as passports or
+                                driver's licenses.</td>
+                        </tr>
+                        <tr>
+                            <td>Document Authentication</td>
+                            <td>Confirm the validity and authenticity of provided identification documents through
+                                manual review or automated verification systems.</td>
+                        </tr>
+                        <tr>
+                            <td>Due Diligence Procedures</td>
+                            <td>Conduct thorough checks to ensure compliance with regulatory requirements and to prevent
+                                fraudulent activities.</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="table-para">
+            <h4>Identification Documentation Needs</h4>
+            <p>Markets has to confirm your identification in order to abide by Australian Anti Money Laundering (AML)
+                and Counter Terrorism rules. While we make every effort to complete this procedure online by utilizing
+                the greatest technology available, there may be occasions when you must send your papers to our back
+                office staff. Please refer to the list below for the required paperwork.</p>
+        </div>
+    </div>
+</section>
+
+<section class="provide-world pt-120 position-relative z-0  pb-60" style="background-color: #000;">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12 col-xxl-10">
+                <div class="heading__content mb-10 mb-lg-15 text-center">
+                    <h2 class="mb-5 mb-lg-6">Documents Required</h2>
+                    <p>Kindly provide a copy of ONE of the following papers proving your identity:</p>
+                </div>
+            </div>
+        </div>
+        <div class="row gy-6 gy-xxl-0">
+            <div class="col-md-6 col-xxl-4">
+                <div class="provide-world__card nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                    <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
+                        <img src="assets/images/icon/number-1.png" alt="time">
+                    </span>
+                    <h5 class="mt-5 mb-5">ID doc arrow ico Passport (must to be valid right now)</h5>
+                </div>
+            </div>
+            <div class="col-md-6 col-xxl-4">
+                <div class="provide-world__card nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                    <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
+                        <img src="assets/images/icon/two.png" alt="wallet">
+                    </span>
+                    <h5 class="mt-5 mb-5">a current driver's license</h5>
+                    <br>
+                </div>
+            </div>
+            <div class="col-md-6 col-xxl-4">
+                <div class="provide-world__card nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                    <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
+                        <img src="assets/images/icon/number-3.png" alt="low-price">
+                    </span>
+                    <h5 class="mt-5 mb-5">Authentic Proof of Age card</h5>
+                    <br>
+                </div>
+            </div>
+        </div>  
+</section>
+
+<section class="provide-world pt-120 position-relative z-0 pb-120" style="background-color: #000;">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12 col-xxl-10">
+                <div class="heading__content mb-10 mb-lg-15 text-center">
+                    <h2 class="mb-5 mb-lg-6">Documents Required</h2>
+                    <p>To confirm your address, kindly include a copy of ONE of the following papers as well:</p>
+                </div>
+            </div>
+        </div>
+        <div class="row gy-6 gy-xxl-0">
+            <div class="col-md-6 col-xxl-4">
+                <div class="provide-world__card nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                    <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
+                        <img src="assets/images/icon/number-1.png" alt="time">
+                    </span>
+                    <h5 class="mt-5 mb-5">Utility Bill (Phone, Electricity, Gas) id doc arrow ico</h5>
+                </div>
+            </div>
+            <div class="col-md-6 col-xxl-4">
+                <div class="provide-world__card nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                    <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
+                        <img src="assets/images/icon/two.png" alt="wallet">
+                    </span>
+                    <h5 class="mt-5 mb-5">Statement from a bank or credit card</h5>
+                </div>
+            </div>
+            <div class="col-md-6 col-xxl-4">
+                <div class="provide-world__card nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
+                    <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
+                        <img src="assets/images/icon/number-3.png" alt="low-price">
+                    </span>
+                    <h5 class="mt-5 mb-5">Tax Document Issued by the Government</h5>
+                </div>
+            </div>
+        </div>  
+</section>
+
 
 <?php include("includes/footer.php"); ?>
