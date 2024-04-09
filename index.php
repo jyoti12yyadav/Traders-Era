41f5719b38da72cf93f88253295166ba9357d695
ramaprixim <rama.prixim@gmail.com>
2024-04-09 16:10:15 +0530

    first commit

--- index.php
+++ index.php
@@ -359,7 +359,7 @@
 
 <iframe
     src="https://fxpricing.com/fx-widget/ticker-tape-widget.php?id=1,2,3,5,14,20&border=show&speed=50&click_target=blank&theme=dark&tm-cr=212529&hr-cr=FFFFFF13&by-cr=28A745&sl-cr=DC3545&flags=circle&d_mode=regular&column=&lang=en&font=Arial, sans-serif"
-    width="100%" height="85" style="border: unset;margin-top:150px"></iframe>
+    width="100%" height="85" style="border: unset;margin-top:132px"></iframe>
 <style type="text/css">
     #fx-pricing-widget-copyright {
         text-align: center;
