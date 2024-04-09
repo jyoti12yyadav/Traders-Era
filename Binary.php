a1e572f44361abead5340c6f9dc535088f9bab4b
pranaliprixim <pranalip.prixim@gmail.com>
2024-04-09 16:07:37 +0530

    content

--- Binary.php
+++ Binary.php
@@ -20,7 +20,7 @@
         <div class="row justify-content-center">
             <div class="col-lg-8 col-xxl-6">
                 <div class="heading__content mb-10 mb-lg-15 text-center">
-                    <h3 class="mb-5 mb-lg-6">Why traders choose us ?</h3>
+                  
                 </div>
             </div>
         </div>
@@ -30,8 +30,18 @@
                     <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
                         <img src="assets/images/icon/time.png" alt="stock-market">
                     </span>
-                    <h4 class="mt-5 mb-5">Quick Deposits &amp; Withdrawals</h4>
-                    <p>Become an TradersEra client within minutes! Register yourself, fund your chosen account, and start stock trading trouble-free.</p>
+                    <h4 class="mt-5 mb-5">Benefits</h4>
+                    <p style="font-weight:bold">Simplicity: Easy-to-understand trading concept.</p>
+                    <hr>
+                    <p style="font-weight:bold"> Potential High Returns: Profitable opportunities with fixed payouts.
+                    </p>
+                    <hr>
+                    <p style="font-weight:bold"> Limited Risk: Defined risk with predetermined investment amounts.</p>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+
                 </div>
             </div>
             <div class="col-md-6 col-xxl-3">
@@ -39,8 +49,17 @@
                     <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
                         <img src="assets/images/icon/customer-service.png" alt="stock-market">
                     </span>
-                    <h4 class="mt-5 mb-5">Unparalleled Customer Support</h4>
-                    <p>With an experienced team backing you up at every difficulty, TradersEra strives to offer you the best possible customer support 24×5.</p>
+                    <h4 class="mt-5 mb-5">How it Works</h4>
+                    <p style="font-weight:bold"> Choose an Asset: Select from various assets such as currencies,
+                        commodities, stocks, or indices.</p>
+                    <hr>
+                    <p style="font-weight:bold"> Predict Market Direction: Decide whether the asset's value will
+                        increase or decrease </p>
+                    <hr>
+
+                    <p style="font-weight:bold"> Place Trade: Enter the amount to invest and execute the trade.
+                    </p>
+                    <br>
                 </div>
             </div>
             <div class="col-md-6 col-xxl-3">
@@ -48,8 +67,15 @@
                     <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
                         <img src="assets/images/icon/investment-insurance.png" alt="stock-market">
                     </span>
-                    <h4 class="mt-5 mb-5">Secure &amp; Safe Funds</h4>
-                    <p>Trade across all our assets with confidence and quit worrying about your funds’ safety.</p>
+                    <h4 class="mt-5 mb-5">Strategies</h4>
+                    <p style="font-weight:bold"> High/Low: Predict whether the asset's value will be higher or lower
+                        than the current price at expiry.</p>
+                    <hr>
+                    <p style="font-weight:bold">One-Touch: Speculate if the asset's price will reach a specific level
+                        before expiry.</p>
+                    <hr>
+                    <p style="font-weight:bold"> Boundary: Determine if the asset's price will stay within a predefined
+                        range.</p>
                     <br>
                     <br>
                 </div>
@@ -59,10 +85,85 @@
                     <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
                         <img src="assets/images/icon/discount.png" alt="stock-market">
                     </span>
-                    <h4 class="mt-5 mb-5">Low Commissions</h4>
-                    <p>Get raw pricing with no markups with TradersEra. Start stock trading with us at lowest commissions possible.</p>
+                    <h4 class="mt-5 mb-5">Risks</h4>
+                    <p style="font-weight:bold"> Potential Losses: Loss of invested capital if the prediction is
+                        incorrect.</p>
+                    <hr>
+                    <p style="font-weight:bold"> Market Volatility: Fluctuations in asset prices may impact trade
+                        outcomes.</p>
+                    <hr>
+                    <p style="font-weight:bold">Scams: Risks associated with unregulated brokers and fraudulent
+                        platforms.
+                    </p>
                     <br>
                     <br>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="trade_on trade_on--secondary pt-120 pb-120 position-relative z-0">
+    <div class="animation position-absolute top-0 left-0 w-100 h-100 z-n1">
+        <img src="assets/images/star3.png" alt="vector"
+            class="position-absolute push_animat d-none d-xxxl-flex top-0 start-0 pt-lg-16 ps-20 ms-lg-8">
+        <img src="assets/images/sun2.png" alt="vector"
+            class="sun2 position-absolute push_animat d-none d-xxl-flex bottom-0 pb-120">
+    </div>
+    <div class="container">
+        <div class="row gy-10 gy-xxl-0 justify-content-center justify-content-xxl-between align-items-center">
+            <div class="col-md-10 col-lg-6 col-xxl-5">
+                <div class="trade_on__content">
+                    <span class="heading p1-color fs-six mb-5"></span>
+                    <h4 class="mb-4 mb-lg-5" style="font-size: 32px;">Binary Trading At TradersEra
+                    </h4>
+                        </p>
+                        <p>  Binary trading simplifies investment by allowing traders to predict whether an asset's value
+                            will rise or fall within a specified timeframe. This trading method offers several benefits,
+                            including simplicity, potential high returns, and limited risk due to fixed payouts and
+                            predetermined investment amounts.</p>
+                        <br>
+                        <p class="mx-ch">To participate in binary trading, traders select an asset, predict its market
+                            direction, set an expiry time, and place their trade. Common strategies include High/Low,
+                            One-Touch, and Boundary options, each offering unique opportunities for profit. However,
+                            binary trading also involves risks, such as potential losses, market volatility, and the
+                            presence of scams.</p>
+                        <br>
+                        <p>Traders should exercise caution, conduct thorough research, and choose reputable brokers to
+                            mitigate these risks and maximize their chances of success.</p>
+                </div>
+            </div>
+            <div class="col-10 col-sm-8 col-md-7 col-lg-6 order-2 order-lg-0">
+                <div class="trade_on__thumbs d-flex justify-content-end">
+                    <img src="assets/images/trading_ecosystem.png" alt="Imgae">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="trade_on trade_on--secondary pt-120 pb-120 position-relative z-0">
+    <div class="animation position-absolute top-0 left-0 w-100 h-100 z-n1">
+        <img src="assets/images/star3.png" alt="vector"
+            class="position-absolute push_animat d-none d-xxxl-flex top-0 start-0 pt-lg-16 ps-20 ms-lg-8">
+        <img src="assets/images/sun2.png" alt="vector"
+            class="sun2 position-absolute push_animat d-none d-xxl-flex bottom-0 pb-120">
+    </div>
+    <div class="container">
+        <div class="row gy-10 gy-xxl-0 justify-content-center justify-content-xxl-between align-items-center">
+            <div class="col-md-10 col-lg-6 col-xxl-5">
+                <div class="trade_on__content">
+                    <span class="heading p1-color fs-six mb-5"></span>
+                    <h4 class="mb-4 mb-lg-5" style="font-size: 32px;">What is Binary Trading?
+                    </h4>
+                        </p>
+                        <p>  Binary trading involves predicting whether the value of an asset will rise or fall within a specified time frame.</p>
+                        <br>
+                       
+                </div>
+            </div>
+            <div class="col-10 col-sm-8 col-md-7 col-lg-6 order-2 order-lg-0">
+                <div class="trade_on__thumbs d-flex justify-content-end">
+                    <img src="assets/images/trading_ecosystem.png" alt="Imgae">
                 </div>
             </div>
         </div>
