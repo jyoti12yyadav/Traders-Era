31ce6b9d61b947212caac73787ceba1c285e9741
jyoti12yyadav <jyoti.prixim@gmail.com>
2024-05-22 10:40:35 +0530

    Header

--- forex-cfds.php
+++ forex-cfds.php
@@ -74,11 +74,11 @@
     <div class="container mt-10 mt-lg-0 pt-15 pt-lg-20 pb-5 pb-lg-0">
         <div class="row">
             <div class="col-12 breadcrumb-area ">
-                <h2 class="mb-4">Account Types</h2>
+                <h2 class="mb-4">Forex CFDs</h2>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0">
                         <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                        <li class="breadcrumb-item ms-2 ps-7 active" aria-current="page"><span>Account Types</span></li>
+                        <li class="breadcrumb-item ms-2 ps-7 active" aria-current="page"><span>Forex CFDs</span></li>
                     </ol>
                 </nav>
             </div>
