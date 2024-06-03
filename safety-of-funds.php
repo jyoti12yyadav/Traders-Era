1acd7db5830a6d3a41187c755ffa3ec4644638ac
Anjaa90 <anjali.prixim@gmail.com>
2024-06-03 16:09:21 +0530

    an

--- safety-of-funds.php
+++ safety-of-funds.php
@@ -2,14 +2,13 @@
 
 
 <style>
-    
-.pb-120 {
-    padding-bottom: 60px !important;
-}
-.pt-120 {
-    padding-top: 60px !important;
-}
+    .pb-120 {
+        padding-bottom: 60px !important;
+    }
 
+    .pt-120 {
+        padding-top: 60px !important;
+    }
 </style>
 
 <section class="banner-section  pt-2 pb-1">
@@ -34,14 +33,14 @@
 <section class="provide-world pt-120 pb-120  position-relative z-0" style="padding-bottom: 25px;">
     <div class="container">
         <div class="row gy-6 gy-xxl-0">
-            <h2 class=" mb-lg-5 d-flex justify-content-center">Ensuring the Safety of Your Funds with TradersEra </h2>
-     
+            <h2 class=" mb-lg-5 d-flex justify-content-center">Ensuring the Safety of Your Funds with TradersEra</h2>
+
             <div class="col-md-6 col-xxl-4 mt-6">
                 <div class="provide-world__card secondary nb3-bg text-center cus-rounded-1 py-5 py-lg-10 px-4 px-lg-9">
                     <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
                         <img src="assets/images/icon/Spreads.png" alt="stock-market">
                     </span>
-                    
+
                     <h3 class="mt-5 mb-5">Segregated Accounts
                     </h3>
                     <p>Your funds are held in segregated bank accounts separate from TradersEra 's operational accounts.
@@ -88,6 +87,51 @@
     </div>
 
 </section>
+<section class="trade_on trade_on--secondary pt-120 pb-120 position-relative z-0">
+    <div class="animation position-absolute top-0 left-0 w-100 h-100 z-n1">
+        <img src="assets/images/star3.png" alt="star3"
+            class="position-absolute push_animat d-none d-xxxl-flex top-0 start-0 pt-lg-16 ps-20 ms-lg-8">
+    </div>
+    <div class="container">
+        <div class="row gy-10 gy-xxl-0 justify-content-center justify-content-xxl-between align-items-center">
+            <div class="col-10 col-sm-8 col-md-7 col-lg-6 order-2 order-lg-0">
+                <div class="trade_on__thumbs d-flex justify-content-end">
+                    <img src="assets/images/tradersera.png" alt="Web Based Tradersera Platform ">
+                </div>
+            </div>
 
+            <div class="col-md-10 col-lg-6 col-xxl-5">
+                <div class="trade_on__content">
+                    <h2 class="mb-4 mb-lg-5" style="font-size:35px;">Client Fund Insurance
+                    </h2>
+                    <p class="mx-ch">In addition to our security measures, TradersEra may also offer client fund
+                        insurance through reputable third-party providers. This insurance coverage provides an extra
+                        layer of protection for your funds in the unlikely event of insolvency or financial distress.
+                    </p>
+                    <br>
+                    <p class="mx-ch">With TradersEra, you can trade with confidence, knowing that your funds are secure
+                        and protected. Our unwavering commitment to the safety and security of your investments
+                        underscores our dedication to providing you with a trustworthy and reliable trading environment.
+                        Trade with peace of mind at TradersEra, where your financial security is our top priority.</p>
+                </div>
+
+
+                <h5 class="mt-4 mb-lg-5">
+                    Risk Management Protocol
+                </h5>
+                <p>To reduce possible losses and increase long-term returns, carefully diversify your portfolio and set
+                    clear stop-loss limits.
+                </p>
+                <h5 class="mt-4 mb-lg-5">
+                    Client funds insurance
+                </h5>
+                <p>Our client funds insurance safeguards your money. Our top aim is to keep you stress-free as we
+                    protect your money from unexpected occurrences.
+                </p>
+
+            </div>
+
+        </div>
+</section>
 
 <?php include("includes/footer.php"); ?>
