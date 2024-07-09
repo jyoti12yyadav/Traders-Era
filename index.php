888ece20b1ed8faad356625f3e1fc68f44a78b0b
jyoti12yyadav <jyoti.prixim@gmail.com>
2024-07-09 19:37:56 +0530

    Header

--- index.php
+++ index.php
@@ -379,7 +379,7 @@
 
 <iframe
     src="https://fxpricing.com/fx-widget/ticker-tape-widget.php?id=1,20,39,19,1984,1975,1972,25,6729&border=show&speed=50&click_target=blank&theme=dark&tm-cr=212529&hr-cr=FFFFFF13&by-cr=28A745&sl-cr=DC3545&flags=circle&d_mode=regular&column=ask,bid,spread&lang=en&font=Arial, sans-serif"
-    width="100%" height="85" style="border: unset;margin-top:132px"></iframe> 
+    width="100%" height="85" style="border: unset;margin-top:132px"></iframe>
 
 
 
@@ -728,7 +728,7 @@
         </div>
     </div>
 </section> -->
-<section class="trade_on trade_on--secondary pt-120 pb-120 position-relative z-0">
+<!-- <section class="trade_on trade_on--secondary pt-120 pb-120 position-relative z-0">
     <div class="animation position-absolute top-0 left-0 w-100 h-100 z-n1">
         <img src="assets/images/star3.png" alt="star3"
             class="position-absolute push_animat d-none d-xxxl-flex top-0 start-0 pt-lg-16 ps-20 ms-lg-8">
@@ -785,7 +785,18 @@
                 </ul>
             </div>
         </div>
-</section>
+</section> -->
+
+
+
+
+
+
+
+
+
+
+
 <section class="provide-world pt-120 position-relative z-0 mb-10 pb-120" style="background-color: #000;">
     <div class="container">
         <div class="row justify-content-center">
