0fb6e91ac89a2b41891b813721798127fe6c2cbf
Anjaa90 <anjali.prixim@gmail.com>
2024-07-11 14:35:14 +0530

    an

---
+++ Download-MT5.php
@@ -0,0 +1,140 @@
+<?php $title = "Binary Options"; include("includes/header.php"); ?>
+<style>
+    .main-table {
+        display: flex;
+    }
+    .table-content {
+        display: flex;
+    }
+    .sub-table-content {
+        width: 100%;
+        padding: 20px 50px;
+        background-color: #000000;
+        border-right: 1px solid #3e3d3d;
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+        font-size: 20px;
+    }
+    .table-content2>.sub-table-content {
+        background-color: #9ad953;
+        border: 1px solid black;
+        border-radius: 2px;
+        color: black;
+        font-weight: 700;
+        border-radius: 10px 10px 0px 0px;
+    }
+    .sub-table-content>span {
+        display: flex;
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+        font-size: 18px;
+        font-weight: 700;
+        padding-bottom: 4px;
+    }
+    .sub-table-content>h4 {
+        color: #000000;
+    }
+    .cus-rounded-3 {
+        border-radius: 0px 0px 10px 10px;
+    }
+    .packages {
+        background: #9ad953;
+        padding: 25px;
+        border-radius: 10px 10px 0px 0px;
+    }
+    .packages>h3 {
+        color: #000000;
+        text-align: center;
+    }
+    .package-para {
+        font-size: 20px;
+    }
+    .pb-120 {
+        padding-bottom: 60px !important;
+    }
+    .pt-120 {
+        padding-top: 60px !important;
+    }
+    .company-story__part {
+        border: solid 1px #9ad953;
+        padding: 30px;
+        border-radius: 19px;
+    }
+    .alt_form select {
+        border-bottom: rgb(52 56 57);
+    }
+    .single-input {
+        margin-top: 27px !important;
+        border-bottom: 2px solid #3a3939;
+    }
+    .nice-select {
+        padding-top: 10px;
+        padding-bottom: 14px;
+    }
+    .nice-select:focus,
+    .nice-select:hover {
+        background-color: transparent;
+        box-shadow: none;
+    }
+    .nice-select .list {
+        background-color: #000;
+    }
+    .pb-120 {
+        padding-bottom: 60px;
+    }
+    .pt-120 {
+        padding-top: 60px;
+    }
+    .referred_image {
+        width: 45px;
+    }
+    .referer_image {
+        width: 50px;
+    }
+</style>
+<!-- jyoti -->
+<section class="banner-section pt-2 pb-1">
+    <div class="container mt-10 mt-lg-0 pt-15 pt-lg-20 pb-5 pb-lg-0">
+        <div class="row">
+            <div class="col-12 breadcrumb-area ">
+                <h1 class="mb-4">Binary Trading</h1>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-0">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item ms-2 ps-7 active" aria-current="page"><span>Binary Trading</span></li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</section>
+</section>
+<section class="trade_on a2-bg pt-120 pb-120 position-relative z-0">
+    <div class="animation position-absolute top-0 left-0 w-100 h-100 z-n1">
+        <img src="assets/images/coin.png" alt="null" class="position-absolute d-none d-md-flex previewShapeRevX">
+        <img src="assets/images/coin_vector.png" alt="null"
+            class="position-absolute d-none d-xxxl-flex bottom-0 end-0 previewShapeRevX opacity-50">
+    </div>
+    <div class="container">
+        <div class="row gy-10 gy-xxl-0 justify-content-center justify-content-xxl-between align-items-center">
+            <div class="col-lg-6 col-xxl-5">
+                <div class="trade_on__content">
+                    <h2 class="mb-4 mb-lg-5">Crypto CFD</h2>
+                    <p class="fs-six mx-ch">Discover the exciting world of Crypto CFDs with our platform. Trade popular
+                        cryptocurrencies such as Bitcoin (BTC), Ethereum (ETH), Ripple (XRP), Litecoin (LTC), and
+                        Bitcoin Cash (BCH) with competitive margin requirements and leverage options. Take advantage of
+                        our user-friendly interface and robust trading tools to capitalize on crypto market movements
+                        effectively.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-8 col-lg-6">
+                <div class="trade_on__thumbs d-flex justify-content-end">
+                    <img src="assets/images/Crypto CFD.png" alt="Trade Crypto CFD on TradersEra Trading Paltform">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
