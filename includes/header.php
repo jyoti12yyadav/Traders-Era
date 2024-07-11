c28fc1bb00d9b9a24ad9197de9c5a3ed3c7f7c65
24dineshkumar <dineshkumar.prixim@gmail.com>
2024-07-11 14:51:09 +0530

    commit changes

--- includes/header.php
+++ includes/header.php
@@ -74,10 +74,14 @@
                     </div>
                 </div>
                 <div class="right-area position-relative  ms-0 d-end gap-1 gap-xl-4 d-none d-lg-flex">
-                    <div class="single-item" style="  margin-right: 10px; ">
+                    <!-- <div class="single-item" style="  margin-right: 10px; ">
                         <a href="javascript:void(Tawk_API.toggle())"
                             class="rotate_eff flex-nowrap py-1 px-2 px-xl-3 d-end gap-1 fw-bold nw-button"> Support </a>
-                    </div>
+                    </div> -->
+                <div class="single-item" style="margin-right: 10px; ">
+                        <a href="https://download.mql5.com/cdn/web/blackridge.trading.ltd/mt5/blackridgetrading5setup.exe"
+                            class="rotate_eff flex-nowrap py-1 px-2 px-xl-3 d-end gap-1 fw-bold nw-button">Download MT5</a>
+                    </div> 
                     <div class="single-item" style="  margin-right: 10px; ">
                         <a href="https://trading.tradersera.com/register"
                             class="rotate_eff flex-nowrap py-1 px-2 px-xl-3 d-end gap-1 fw-bold nw-button" target="new">
