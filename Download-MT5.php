5cee318ed331bb080086663d017584d0a80f968f
Anjaa90 <anjali.prixim@gmail.com>
2024-07-12 19:01:58 +0530

    an

--- Download-MT5.php
+++ Download-MT5.php
@@ -186,8 +186,26 @@
                         </span>
                         <h4 class="mt-5 mb-5" >Download MT5 for Android</h4>
                         <p style="padding-left:50px; padding-right: 50px;">Access market data, news, charting tools, and essential trading features on the go using the MT5 Android app on your smartphone or tablet</p>
-                        
+                        <a href="#" class="border-button" style="margin-top: 10px;">Click Me</a>
+
                     </div>
+                    <style>
+                        .border-button {
+    display: inline-block;
+    padding: 10px 20px;
+    border: 2px solid #9AD953; /* Border color */
+    color: #9AD953; /* Text color */
+    text-decoration: none;
+    border-radius: 4px;
+    transition: color 0.3s ease, border-color 0.3s ease;
+}
+
+.border-button:hover {
+    color: #9AD951; /* Text color on hover */
+    border-color: #9AD951; /* Border color on hover */
+}
+
+                    </style>
                     <div class="col-md-4 provide-world__card text-center nb3-bg py-5 px-4 px-lg-5">
                     
                         <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
@@ -196,6 +214,7 @@
                         <h4 class="mt-5 mb-5">Download MT5 for iOS</h4>
                         <p style="padding-left:50px; padding-right: 50px;" >Download MetaTrader 5 on your iOS device to start trading on the go. This versatile app is compatible with all iOS devices,
                              giving you the freedom to trade anytime, anywhere.</p>
+                             <a href="#" class="border-button" style="margin-top: 10px;">Click Me</a>
                     </div>
                     <div class="col-md-4 provide-world__card text-center nb3-bg py-5 px-4 px-lg-5">
                         <span class="provide-card__icon d-center nb4-bg p-4 rounded-circle mx-auto">
@@ -204,6 +223,7 @@
                         <h4 class="mt-5 mb-5" style="padding-left: 50px; padding-right: 50px;">Download MT5 for Mac </h4>
                         <p style="padding-left:50px; padding-right: 50px;">Are you using a Mac? You can get a really strong desktop program for your computer! 
                             Just get the MT5 terminal for Mac OS X, set it up, and start trading!</p>
+                            <a href="#" class="border-button" style="margin-top: 10px;">Click Me</a>
                     </div>
                 </div>
             </div>
