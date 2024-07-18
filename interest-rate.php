fda783babfd3ac4affda1d2ac6190c68c010e2a0
Anjaa90 <anjali.prixim@gmail.com>
2024-07-18 19:26:02 +0530

    an

--- interest-rate.php
+++ interest-rate.php
@@ -22,6 +22,121 @@
         </div>
     </div>
 </section>
+
+<section class="trade_on a2-bg pt-120 pb-120 position-relative z-0">
+    <div class="animation vector position-absolute top-0 left-0 w-100 h-100 z-n1">
+        <img src="assets/images/star3.png" alt="vector"
+            class="position-absolute top-0 end-0 pe-15 pt-20 push_animat d-none d-xxxl-flex">
+        <img src="assets/images/vector.png" alt="vector"
+            class="position-absolute top-0 start-0 pt-20 ms-20 jello d-none d-xxxl-flex">
+        <img src="assets/images/vector5.png" alt="vector"
+            class="position-absolute bottom-0 end-0 d-none d-lg-flex previewShapeRevX ">
+    </div>
+    <div class="container">
+        <div class="row gy-10 gy-xxl-0 justify-content-center justify-content-xxl-between align-items-center">
+            <div class="col-10 col-sm-8 col-md-7 col-lg-6 order-2 order-lg-0">
+                <div class="trade_on__thumbs d-flex justify-content-end">
+                    <img src="assets/images/tradersera.png" alt=" Learn about Forex Trading">
+                </div>
+            </div>
+            <div class="col-lg-6 col-xxl-5">
+                <div class="trade_on__content">
+                    <h2 class="mb-4 mb-lg-5">What is interest rates?</h2>
+                    <p class="fs-six mx-ch">Interest rates are the cost of borrowing money or the return on savings, 
+                        expressed as a percentage. They can be fixed (remaining constant throughout the loan period) 
+                        or variable (fluctuating over time). </p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Trade On end -->
+<section class="trade_on a2-bg pt-120 pb-120 position-relative z-0">
+    <div class="animation position-absolute top-0 left-0 w-100 h-100 z-n1">
+        <img src="assets/images/coin.png" alt="vector" class="position-absolute d-none d-md-flex previewShapeRevX">
+    </div>
+    <div class="container">
+        <div class="row gy-10 gy-xxl-0 justify-content-center justify-content-xxl-between align-items-center">
+            <div class="col-lg-7 col-xxl-6">
+                <div class="trade_on__content">
+                    <!-- <span class="heading s1-color fs-five mb-5">Trade On Our</span> -->
+                    <h2 class="mb-4 mb-lg-5">Influencing Factors
+                    </h2>
+                    <ul class="d-flex gap-4 flex-column mt-6">
+                        <li class="d-flex align-items-center gap-3 fs-six-up" style="font-weight: 600;"><i
+                                class="ti ti-circle-check s1-color fs-four"></i><a href="#question1">Central Bank Policies:</a>
+                                <p>Affect overall economic stability.</p> </li>
+
+                        <li class="d-flex align-items-center gap-3 fs-six-up" style="font-weight: 600;"><i
+                                class="ti ti-circle-check s1-color fs-four"></i><a href="#question2">Inflation:</a>
+                                <p>Higher inflation typically leads to higher rates</p>
+                        </li>
+                        <li class="d-flex align-items-center gap-3 fs-six-up" style="font-weight: 600;"><i
+                                class="ti ti-circle-check s1-color fs-four" style="font-weight: 600;"></i><a href="#question3">Economic Conditions:</a>
+                                 <p>Strong economies may see rising rates.</p></li>
+                        <li class="d-flex align-items-center gap-3 fs-six-up" style="font-weight: 600;"><i
+                                class="ti ti-circle-check s1-color fs-four" style="font-weight: 600;"></i><a href="#question4">Credit Risk:</a>
+                                <p>Higher risk borrowers often face higher rates.</p></li>
+                        
+                    </ul>
+                    <br>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6">
+                <div class="trade_on__thumbs d-flex justify-content-end">
+                    <img src="assets/images/traders- last.png" alt="Forex trading for beginners">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="trade_on trade_on--secondary pt-120 pb-120 position-relative z-0">
+    <div class="animation position-absolute top-0 left-0 w-100 h-100 z-n1">
+        <img src="assets/images/star3.png" alt="vector"
+            class="position-absolute push_animat d-none d-xxxl-flex top-0 start-0 pt-lg-16 ps-20 ms-lg-8">
+    </div>
+    <div class="container">
+        <div class="row gy-10 gy-xxl-0 justify-content-center justify-content-xxl-between align-items-center">
+            <div class="col-10 col-sm-8 col-md-7 col-lg-6 order-2 order-lg-0">
+                <div class="trade_on__thumbs d-flex justify-content-end">
+                    <img src="assets/images/Advantages for introducing brokers.png"
+                        alt="TradersEra's Global markets, liquidity, low spreads, swap-free">
+                </div>
+            </div>
+            <div class="col-md-10 col-lg-6 col-xxl-5">
+                <div class="trade_on__content">
+                    <h2 class="mb-4 mb-lg-5" style="font-size:35px;">Affecting factors and impacts
+                    </h2>
+                    <!-- <h3 class="mb-4 mb-lg-5">An Introducing Broker
+                    </h3> -->
+                    <p>Several factors affect interest rates, including central bank policies, inflation,
+                         economic conditions, and credit risk. Central banks set benchmark rates to stabilize 
+                         the economy, while inflation often leads to higher rates to maintain purchasing power. 
+                         Understanding interest rates is crucial for making informed financial decisions, 
+                         whether you're borrowing, saving, or investing, as they significantly impact personal 
+                         finance and overall economic growth.</p>
+                </div>
+                <!-- <ul class="d-flex gap-4 flex-column mt-6">
+                    <li class="d-flex align-items-center gap-3 fs-six-up"><i
+                            class="ti ti-circle-check s1-color fs-four"></i>MetaTrader5, the award-winning trading
+                        platform, provides a range of advanced features to enhance
+                        your trading experience. With its Strategy Tester, OrderBook, Built-in technical indicators, and
+                        graphics tools, you have everything you need to make informed trading decisions.
+                    </li>
+                    <li class="d-flex align-items-center gap-3 fs-six-up"><i
+                            class="ti ti-circle-check s1-color fs-four"></i>Explore a wide range of global markets with
+                        CFDs: trade shares, stocks, commodities, gold, oil, forex, cryptocurrencies, and much more.
+                    </li>
+                    <li class="d-flex align-items-center gap-3 fs-six-up"><i
+                            class="ti ti-circle-check s1-color fs-four"></i>Our full range of global markets offers your
+                        customers exceptional liquidity, low spreads, and swap-free conditions for their benefit.
+                    </li>
+                    <br>
+                </ul> -->
+            </div>
+        </div>
+</section>
 <section class="company-story position-relative z-0  pt-120 pb-120">
     <div class="container">
     <div class="row justify-content-center text-center mb-10 mb-lg-15">
