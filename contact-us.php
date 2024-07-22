e060d9756ed5143afb5cd4fe835f1e404f10c254
jyoti12yyadav <jyoti.prixim@gmail.com>
2024-07-22 13:08:50 +0530

    Initial Commit

--- contact-us.php
+++ contact-us.php
@@ -42,7 +42,7 @@
     <div class="container mt-10 mt-lg-0 pt-15 pt-lg-20 pb-5 pb-lg-0">
         <div class="row">
             <div class="col-12 breadcrumb-area ">
-                <h1 class="mb-1">We are here for you, 24/5.</h1>
+                <h1 class="mb-1">We are here for you, 24/5</h1>
                 <nav aria-label="breadcrumb">
                     <a href="signup.html"
                         class="cmn-btn fs-six-up nb4-xxl-bg gap-2 gap-lg-3 align-items-center py-2 py-lg-3 px-lg-5 mt-7 mt-xxl-8"
